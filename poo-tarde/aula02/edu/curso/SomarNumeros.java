2dfa8b2b8dd616722e90d914068e9d290b9c0d12
Antonio Carvalho <antoniorcn@hotmail.com>
2024-08-15 17:11:09 -0300

    poo tarde parte 1

---
+++ poo-tarde/aula02/edu/curso/SomarNumeros.java
@@ -0,0 +1,18 @@
+package edu.curso;
+import java.util.Scanner;
+// class Teste { 
+
+// }
+
+public class SomarNumeros { 
+    public static void main(String[] args) { 
+        Scanner input = new Scanner(System.in);
+        System.out.println("Por favor digite um número: ");
+        double num1 = input.nextDouble();
+        System.out.println("Por favor digite outro número: ");
+        double num2 = input.nextDouble();
+        double resultado = num1 + num2;
+        System.out.printf("Resultado: %6.2f%n", resultado);
+        input.close();
+    }
+}
