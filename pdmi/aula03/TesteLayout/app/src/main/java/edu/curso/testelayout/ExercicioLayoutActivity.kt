8cb8bf14c7facef6de0188cc28cf1e9209781b3f
Antonio Carvalho <antoniorcn@hotmail.com>
2024-08-21 17:19:01 -0300

    LES Aula 03

---
+++ pdmi/aula03/TesteLayout/app/src/main/java/edu/curso/testelayout/ExercicioLayoutActivity.kt
@@ -0,0 +1,13 @@
+package edu.curso.testelayout
+
+import android.app.Activity
+import android.os.Bundle
+
+class ExercicioLayoutActivity: Activity() {
+
+    override fun onCreate(bundle: Bundle?) {
+        super.onCreate(bundle)
+
+        setContentView(R.layout.exercicio_botoes_layout)
+    }
+}
