9487b510537608fc24d333666bd3a14e6f79b26c
Antonio Carvalho <antoniorcn@hotmail.com>
2024-09-11 21:22:04 -0300

    LES aula 06

---
+++ les/aula06/agendacontato/src/main/java/edu/curso/agendacontato/AgendaDeContatoApplication.java
@@ -0,0 +1,13 @@
+package edu.curso.agendacontato;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendaDeContatoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendaDeContatoApplication.class, args);
+	}
+
+}
