fa3088b046f1b5e8d876e65b66e1908a3cce2f4a
Antonio Carvalho <antoniorcn@hotmail.com>
2024-09-18 09:28:51 -0300

    pdmi aula07

---
+++ pdmi/aula07/AgendaContato/app/src/main/java/edu/curso/agendacontato/activity/ContatoListaActivity.kt
@@ -0,0 +1,13 @@
+package edu.curso.agendacontato.activity
+
+import android.app.Activity
+import android.os.Bundle
+import edu.curso.agendacontato.R
+
+class ContatoListaActivity : Activity() {
+
+    override fun onCreate(bundle: Bundle?) {
+        super.onCreate(bundle)
+        setContentView(R.layout.contato_lista_activity)
+    }
+}
