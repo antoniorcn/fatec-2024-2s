d970ee7e6d2aeb62892866de1d141e9bc613df91
Antonio Carvalho <antoniorcn@hotmail.com>
2024-09-25 17:08:07 -0300

    LES aula08

---
+++ les/aula08/agendacontato-rest/src/test/java/edu/curso/agendacontato/AgendacontatoApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.curso.agendacontato;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendacontatoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
