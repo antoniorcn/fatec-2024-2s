3105620585d4436ebc526e834d5c6f787894cdce
Antonio Carvalho <antoniorcn@hotmail.com>
2024-10-03 17:18:52 -0300

    POO Tarde aula 09 parte 2

--- poo-tarde/aula09/edu/curso/ContatoBoundary.java
+++ poo-tarde/aula09/edu/curso/ContatoBoundary.java
@@ -5,6 +5,8 @@ import java.util.List;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
@@ -18,8 +20,8 @@ public class ContatoBoundary extends Application {
     private TextField txtNome = new TextField();
     private TextField txtTelefone = new TextField();
     private TextField txtEmail = new TextField();
-    private List<Contato> lista = new ArrayList<Contato>();
-
+    private ContatoControl control = new ContatoControl();
+ 
     @Override
     public void start(Stage stage) { 
         GridPane pane = new GridPane();
@@ -45,15 +47,30 @@ public class ContatoBoundary extends Application {
         pane.add(txtEmail, 1, 2);
 
         Button btnGravar = new Button("Gravar");
-        btnGravar.setOnAction( e -> { lista.add(boundaryToEntity()); } );
+        btnGravar.setOnAction( e -> { 
+            control.gravar( boundaryToEntity() );
+            Alert alert = new Alert(AlertType.INFORMATION);
+            alert.setTitle("Agenda de Contatos");
+            alert.setHeaderText("Informativo");
+            alert.setContentText("Contato foi salvo com sucesso");
+            alert.show();
+            txtNome.setText("");
+            txtTelefone.setText("");
+            txtEmail.setText("");
+        });
         Button btnPesquisar = new Button("Pesquisar");
-        btnPesquisar.setOnAction( e -> {
-            for (Contato c : lista) { 
-                if (c.getNome().contains(txtNome.getText())) {
-                    entityToBoundary(c);
-                }
+        btnPesquisar.setOnAction( e -> { 
+            Contato c = control.pesquisar(txtNome.getText());
+            if (c != null) {
+                entityToBoundary( c ); 
+            } else { 
+                Alert alert = new Alert(AlertType.WARNING);
+                alert.setTitle("Agenda de Contatos");
+                alert.setHeaderText("Aviso");
+                alert.setContentText("Não foi encontrado um contato com este nome");
+                alert.show();                
             }
-        } );
+        });
         pane.add(btnGravar, 0, 3);
         pane.add(btnPesquisar, 1, 3);
 
@@ -71,9 +88,11 @@ public class ContatoBoundary extends Application {
     }
 
     public void entityToBoundary( Contato c ){ 
-        txtNome.setText( c.getNome() );
-        txtTelefone.setText( c.getTelefone() );
-        txtEmail.setText( c.getEmail() );
+        if (c != null) {
+            txtNome.setText( c.getNome() );
+            txtTelefone.setText( c.getTelefone() );
+            txtEmail.setText( c.getEmail() );
+        }
     }
 
 }
