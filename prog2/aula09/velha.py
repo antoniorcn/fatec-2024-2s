6aa42f3d1767c31c1ebd951ec13f9f99dab0d87a
Antonio Carvalho <antoniorcn@hotmail.com>
2024-10-19 08:39:30 -0300

    aula09 parte 1

---
+++ prog2/aula09/velha.py
@@ -0,0 +1,34 @@
+import pygame
+from pygame.locals import QUIT
+# Valor 1 - Jogador 1      2 - Jogador 2      0 - Vazio
+matriz = [
+# colunas    0  1  2
+            [0, 0, 0],      # linha 0
+            [0, 0, 0],      # linha 1
+            [0, 0, 0]       # linha 2
+]
+
+pygame.init()
+
+screen = pygame.display.set_mode( (800, 600), 0, 32 )
+
+largura = 200
+inicio_x = 100
+
+sair = False
+while not sair:
+    # Calcular as regras
+
+    # Mostrar na tela
+    screen.fill( (0, 0, 0) )        # Limpar a tela
+    for coluna in range(3):
+        x = inicio_x + (coluna * largura)
+        pygame.draw.rect(screen, (255, 255, 0),
+                        ( (x, 100), (largura, 50) ), 3  )
+
+    pygame.display.update()         # Atualiza a tela
+
+    # Capturar os eventos
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            sair = True
