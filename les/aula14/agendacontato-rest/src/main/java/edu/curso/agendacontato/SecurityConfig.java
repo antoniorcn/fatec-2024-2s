3fbaa1d2f7bd8fac89732e30ff0f97e445ba8e9a
Antonio Carvalho <antoniorcn@hotmail.com>
2024-11-06 16:17:57 -0300

    LES aula 14 parte 2

--- les/aula14/agendacontato-rest/src/main/java/edu/curso/agendacontato/SecurityConfig.java
+++ les/aula14/agendacontato-rest/src/main/java/edu/curso/agendacontato/SecurityConfig.java
@@ -2,7 +2,9 @@ package edu.curso.agendacontato;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -10,6 +12,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
@@ -19,6 +22,21 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder encoder() { 
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) 
+                                                        throws Exception{
+        http
+            .csrf((csrf) -> csrf.disable())
+            .authorizeHttpRequests(
+                (authorize)->{
+                    authorize.requestMatchers("**").permitAll();
+                    authorize.anyRequest().authenticated();
+                }
+            )
+            .httpBasic(Customizer.withDefaults());
+        return http.build();
     }
 
 
