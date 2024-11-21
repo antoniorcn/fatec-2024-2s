3f304863da7045e7f62700ca0916f17e619c7ebe
Antonio Carvalho <antoniorcn@hotmail.com>
2024-11-21 15:52:43 -0300

    POO Tarde aula 16 parte 3

--- poo-tarde/aula16/compilar.bat
+++ poo-tarde/aula16/compilar.bat
@@ -1,4 +1,4 @@
 rmdir /S /Q ".\build\"
 mkdir build
-javac -d ./build -cp .;./lib/mariadb-java-client-3.5.0.jar ./src/edu/curso/*.java
-java -cp .;./build;./lib/mariadb-java-client-3.5.0.jar %1
+javac -d ./build -cp .;./lib/mariadb-java-client-3.5.1.jar ./src/edu/curso/*.java
+java -cp .;./build;./lib/mariadb-java-client-3.5.1.jar %1
